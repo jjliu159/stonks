064ef262afc6e5c5077cfa41939f92372d9959b1
jjliu159 <55606804+jjliu159@users.noreply.github.com>
2020-08-09 19:13:02 -0400

    separated files into different .py to allow for better readability

--- cnbc headlines.py
+++ cnbc headlines.py
@@ -1,34 +1,31 @@
 import requests
 from bs4 import BeautifulSoup,SoupStrainer
 
-
-stop = False
-
-all_ticker = []
-
-
-while stop == False:
-    symbol = input(str("Enter ticker:"))
-    if symbol == "end" or symbol == "":
-        break;
-    all_ticker.append(symbol.upper())
+cnbc_news = []
 
 cnbc = "https://www.cnbc.com/quotes/?symbol=" #cnbc
 
-for i in range(len(all_ticker)):
-    all_news = []
-    all_time = []
-    
-    cnbc_url = cnbc + all_ticker[i]
-    source_code = requests.get(cnbc_url).text
-    soup = BeautifulSoup(source_code, "lxml")
-    cnbc_headlines = soup.find_all("div",class_ = "subsection assets", id="quote_latest_from_cnbc_summary")
+def grab(all_ticker):
+    for i in range(len(all_ticker)):
+        all_news = []
+        all_time = []
 
-    for headlines in cnbc_headlines:
-        subtitles = headlines.find_all("div", class_ = "assets")
-        for subtitle in subtitles:
-            title = subtitle.find("span").get_text()
-            time = subtitle.find("span", class_ = "note").get_text()
-            if title != "":
-                all_news.append(title)
-                all_time.append(time)
+        cnbc_url = cnbc + all_ticker[i]
+        source_code = requests.get(cnbc_url).text
+        soup = BeautifulSoup(source_code, "lxml")
+        cnbc_headlines = soup.find_all("div",class_ = "subsection assets", id="quote_latest_from_cnbc_summary")
+
+        for headlines in cnbc_headlines:
+            subtitles = headlines.find_all("div", class_ = "assets")
+            for subtitle in subtitles:
+                title = subtitle.find("span").get_text()
+                time = subtitle.find("span", class_ = "note").get_text()
+                if title != "":
+                    if "hr" in time: #check for most recent, the same day
+                        all_news.append(title)
+                        all_time.append(time)
+
+    for k in range(len(all_news)):
+        cnbc_news.append((all_time[k]+", "+all_news[k]))
+
+    return cnbc_news
--- wsj headlines.py
+++ wsj headlines.py
@@ -1,44 +1,35 @@
 import requests
 from bs4 import BeautifulSoup,SoupStrainer
 
-stop = False
-
-all_ticker = []
-
-
-while stop == False:
-    symbol = input(str("Enter ticker:"))
-    if symbol == "end" or symbol == "":
-        break;
-    all_ticker.append(symbol.upper())
+wsj_news = []
 
 wsj = "https://www.wsj.com/market-data/quotes/" #wsj
-#apple_wsj = "https://www.wsj.com/market-data/quotes/AAPL" #test
-headers = {'User-Agent': 'Mozilla/5.0'}
+headers = {'User-Agent': 'Mozilla/5.0'} #need header so that it doesnt reutnr page error for wsj
 
-for i in range(len(all_ticker)):
-    all_news = []
-    all_time = []
+def grab(all_ticker):
+    for i in range(len(all_ticker)): #Wall Street Journal
+        all_news = []
+        all_time = []
 
-    wsj_url = wsj + all_ticker[i]
-    source_code = requests.get(wsj_url,headers=headers).text
-    soup = BeautifulSoup(source_code, "lxml")
-    wsj_headlines = soup.find_all("div", class_ = "contentFrame")
+        wsj_url = wsj + all_ticker[i]
+        source_code = requests.get(wsj_url,headers=headers).text
+        soup = BeautifulSoup(source_code, "lxml")
+        wsj_headlines = soup.find_all("div", class_ = "contentFrame")
 
+        for headlines in wsj_headlines:
+            li_headlines = headlines.find_all("li")
+            for li in li_headlines:
+                res_time = li.find("li", class_ = "cr_dateStamp")#get the text from time <li>
 
-    for headlines in wsj_headlines:
-        li_headlines = headlines.find_all("li")
-        for li in li_headlines:
-            res_time = li.find("li", class_ = "cr_dateStamp")#get the text from time <li>
+                if res_time:
+                    if "hours" in res_time.get_text(): #check for most recent, the same day
+                        all_time.append(res_time.get_text())
+                        res_headline = li.find("span", class_ = "headline") #gets the headlines
+                        if res_headline:
+                            res_title = res_headline.find("a").get_text()
+                            all_news.append(res_title)
 
-            if res_time:
-                all_time.append(res_time.get_text())
-            res_headline = li.find("span", class_ = "headline") #gets the headlines
-            if res_headline:
-                res_title = res_headline.find("a").get_text()
-                all_news.append(res_title)
-
-
-    print("Ticker: ",all_ticker[i])
     for k in range(len(all_news)):
-        print(all_time[k]+", "+all_news[k])
+        wsj_news.append((all_time[k]+", "+all_news[k]))
+
+    return wsj_news
